bbb6710ced46cccd242aa2967765cace97e05a38
Grinko-Oleg <Grinko2020@yandex.by>
2024-08-01 17:02:44 +0300

    Update Exe2.java

--- src/HomeWork/Exe2.java
+++ src/HomeWork/Exe2.java
@@ -20,40 +20,7 @@ public class HomeworkLesson2 {
 
 
 
-        //У нас есть машина. В данной машине есть перечень проверок, перед запуском двигателя
-        //Количество топлива
-        double fuel = 10;
-        //Работоспособен или нет двигатель
-        boolean isEngineWork = true;
-        //Есть ли ошибки в компьютере (false - ошибок нет)
-        boolean hasErrors = false;
-        //Датчики давления шин
-        boolean isWheelWork1 = true;
-        boolean isWheelWork2 = true;
-        boolean isWheelWork3 = true;
-        boolean isWheelWork4 = true;
-
-        //Поменять(убрать, поставить) логические операторы так, чтобы машина запускалась:
-        // когда топлива не меньше 10 литров, двигатель работает, колеса работают, нет ошибок
-        //В ином случае, машина не должна запускаться
-        if (
-                fuel < 10
-                        ||  (!isWheelWork1 || isWheelWork2 || isWheelWork3 || isWheelWork4)
-                        && hasErrors
-                        || isEngineWork
-        ) {
-            System.out.println("Машина едет");
-        } else {
-            System.out.println("Машина не едет");
-        }
-
-
-
-
-
-
-
-
+      
 
 
 
